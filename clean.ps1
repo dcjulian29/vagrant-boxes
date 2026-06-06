5152724520a91ea34e991209b17a8da80658e98b
Julian Easterling <julian@julianscorner.com>
2026-06-06 01:06:08 -0400

    Updated the two clean scripts to remove all build output artifacts

--- clean.ps1
+++ clean.ps1
@@ -1,7 +1,3 @@
-# =============================================================================
-# clean.ps1 - Clean Build Output Directories (Windows).
-# =============================================================================
-
 Remove-Item -Path "boxes"        -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "packer_cache" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "tmp"          -Recurse -Force -ErrorAction SilentlyContinue
--- clean.sh
+++ clean.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-# =============================================================================
-# clean.sh - Clean Build Output Directories (Linux / macOS).
-# =============================================================================
 set -euo pipefail
 
 rm -rf boxes/
+rm -rf packer_cache/
 rm -rf tmp/
 rm -f *.log
